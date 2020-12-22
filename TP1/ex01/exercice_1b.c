b5e18f70ae70be328571677895a41845699a8386
MarouaElga <maroua.elgassousi@um6p.ma>
2020-12-22 21:26:33 +0100

    exe1b

--- TP1/ex01/exercice_1b.c
+++ TP1/ex01/exercice_1b.c
@@ -10,10 +10,20 @@ float *remontee(float A[N][N], float B[N], int n)
   ** https://gribblelab.org/CBootCamp/7_Memory_Stack_vs_Heap.html
   */
 
-  /******Implement the solution here******/
-  /***************************************/
-  
-  /***************************************/
+  float   *x;
+
+  /* Here we allocate the result verctor \
+  (you're gonna do it yourself in the next exercises)*/
+  x = malloc(sizeof(float) * n);
+  x[n-1]=B[n-1]/A[n-1][n-1];
+  for(int i=n-2; i>=0;i--){
+    float s=0;
+    for(int j=i; j<=n; j++){
+            s=s+A[i][j]*x[j];
+    }
+    x[i]=(B[i]-s)/A[i][i];
+    }    
+
   return x;
 }
 
